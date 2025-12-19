91f6d5031f8154939b1f9b7be8f0bb5f3fa8d3d0
Arka Palit <arkapalit55@gmail.com>
2025-12-19 19:50:16 +0530

    Added lookup/search endpoint and fixed post vitals decimal issue

---
+++ EasyHMSAPI.Application/RequestModels/QueryRequestModels/SearchLookupDataRequestModel.cs
@@ -0,0 +1,15 @@
+﻿using EasyHMSAPI.Application.ResponseModels.QueryResponseModels;
+using MediatR;
+using System.Diagnostics.CodeAnalysis;
+
+namespace EasyHMSAPI.Application.RequestModels.QueryRequestModels
+{
+    [ExcludeFromCodeCoverage]
+    public class SearchLookupDataRequestModel : IRequest<SearchLookupDataResponseModel>
+    {
+        public Guid HospitalId { get; set; }
+        public Guid DoctorId { get; set; }
+        public string? LookupType { get; set; }
+        public string? SearchText { get; set; }
+    }
+}
